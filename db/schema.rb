9121d7cfaf81d97cae8b4f09ebe160ecbfa8c094
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-01 21:19:40 +0300

    make_sickness_model : add migration for sickness model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_175018) do
+ActiveRecord::Schema.define(version: 2020_06_01_181157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2020_06_01_175018) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "position"
+  end
+
+  create_table "sicknesses", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "statuses", force: :cascade do |t|
