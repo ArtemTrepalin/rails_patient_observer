35be6effecb3d07ca9799107c677e328ebb16cd8
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-01 20:03:43 +0300

    mak_status_model : add migration for status table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_161552) do
+ActiveRecord::Schema.define(version: 2020_06_01_165211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_06_01_161552) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "watchers", force: :cascade do |t|
