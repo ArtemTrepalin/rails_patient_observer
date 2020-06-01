9d7f1ec669765496a66003608fdcb1122175cded
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-01 17:04:43 +0300

    master : fix welcome view with test

--- test/controllers/web/welcome_controller_test.rb
+++ test/controllers/web/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Web::WelcomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get welcome page' do
-    get root_path
+    get new_admin_session_path
     assert_response :success
   end
 end
