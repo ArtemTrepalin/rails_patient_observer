d0ee4e6948d0aa8959e10572be89ad20fc5e81b4
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-01 17:00:00 +0300

    master : add migration for new admin model

--- db/migrate/20200404090438_create_admins.rb
+++ db/migrate/20200404090438_create_admins.rb
@@ -1,9 +1,7 @@
 class CreateAdmins < ActiveRecord::Migration[6.0]
   def change
     create_table :admins do |t|
-      t.string :name, null: false
       t.string :email, null: false
-      t.string :role, null: false
       t.string :password_digest, null: false
 
       t.timestamps
