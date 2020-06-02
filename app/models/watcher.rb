133ceda3803a24005daeb4c84bf745ca14e2a97d
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-02 21:26:22 +0300

    make_patient_model : add has_many realations with patient model

--- app/models/watcher.rb
+++ app/models/watcher.rb
@@ -1,4 +1,6 @@
 class Watcher < ApplicationRecord
+  has_many :patient, dependent: :nullify
+
   validates :first_name, :last_name, :phone_number, :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :email, uniqueness: true, email: true
