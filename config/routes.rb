46784e1487abcb480493ac58a388611b4d80de15
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-03 15:06:05 +0300

    make_status_crud : create route for statuses crud

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :admin do
       resource :session, only: [:new, :create, :destroy]
       resources :admins
+      resources :statuses
       root 'welcome#index'
     end
   end
