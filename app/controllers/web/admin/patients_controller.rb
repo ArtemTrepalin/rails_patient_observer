b03bd8367e4b767d2492492a5d5bec00530b749d
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-04 23:23:44 +0300

    make_patient_crud : create patients controller

---
+++ app/controllers/web/admin/patients_controller.rb
@@ -0,0 +1,41 @@
+class Web::Admin::PatientsController < Web::Admin::ApplicationController
+  def index
+    @patients = Patient.all
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_attrs)
+    if @patient.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    if @patient.update(patient_attrs)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def patient_attrs
+    params.require(:patient).permit(:first_name, :last_name, :sur_name, :phone_number, :status_id, :street_id, :watcher_id)
+  end
+end
