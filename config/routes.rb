9efcf612fe7a10a7a994c2092bf8ff498c11b9fe
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-04 21:31:09 +0300

    make_street_crud : create routes for street crud

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resources :watchers
       resources :sicknesses
       resources :buildings
+      resources :streets
       root 'welcome#index'
     end
   end
