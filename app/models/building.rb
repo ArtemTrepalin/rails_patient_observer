792c4e907f536f78b409385162678d4d28674e00
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-06 23:08:34 +0300

    remove_street_and_building : remove building model, crud, tests and views

--- app/models/building.rb
+++
@@ -1,4 +0,0 @@
-class Building < ApplicationRecord
-  has_many :streets, through: :street_buildings
-  validates :position, presence: true, uniqueness: true
-end
--- test/factories/buildings.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :building do
-    position
-  end
-end
