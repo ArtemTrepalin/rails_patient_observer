82202d067da7a328ce6aabc2950dc5e7687bc9be
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-12 14:09:10 +0300

    master : codefix by rubocop

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::AuthenticationController < Api::V1::ApplicationController
   before_action :authorize_request, except: :login
 
   def login
-    @watcher = Watcher.find_by_email(params[:email])
+    @watcher = Watcher.find_by(email: params[:email])
     if @watcher&.authenticate(params[:password])
       token = JsonWebToken.encode(watcher_id: @watcher.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+      time = Time.zone.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
                      first_name: @watcher.first_name }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
--- app/observers/patient_observer.rb
+++ app/observers/patient_observer.rb
@@ -2,7 +2,7 @@ class PatientObserver < ActiveRecord::Observer
   observe :patient
 
   def after_update(record)
-    history = History.new({watcher_id: record.watcher_id, status_id: record.status_id, patient_id: record.id})
+    history = History.new({ watcher_id: record.watcher_id, status_id: record.status_id, patient_id: record.id })
     history.save
   end
 end
