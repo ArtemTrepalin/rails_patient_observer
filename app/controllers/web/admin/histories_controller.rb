32c3f9126f2731c7e2ffabe842ae50cf8ab693f7
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-13 14:18:22 +0300

    remove_useless_web_cruds : remove histories crud with views and tests

--- app/controllers/web/admin/histories_controller.rb
+++
@@ -1,9 +0,0 @@
-class Web::Admin::HistoriesController < Web::Admin::ApplicationController
-  def index
-    @histories = History.all
-  end
-
-  def show
-    @history = History.find(params[:id])
-  end
-end
