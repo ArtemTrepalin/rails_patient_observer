2bea830b4ce17182058108a3d2ab67ee7d10456f
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-13 13:45:45 +0300

    make_history_api_crud : add histories api crud

---
+++ app/controllers/api/v1/histories_controller.rb
@@ -0,0 +1,43 @@
+
+class Api::V1::HistoriesController < Api::V1::ApplicationController
+  before_action :authorize_request, except: :create
+  before_action :find_history, except: %i[create index]
+
+  def index
+    @histories = History.all
+    render json: @historys, status: :ok
+  end
+
+  def show
+    render json: @history, status: :ok
+  end
+
+  def create
+    @history = History.new(history_params)
+    if @history.save
+      render json: @history, status: :created
+    else
+      render json: { errors: @history.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def update
+    unless @history.update(history_params)
+      render json: { errors: @history.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @history.destroy
+  end
+
+  private
+
+  def history_params
+    params.permit(
+      :patient_id, :status_id, :temperature, :pressure
+    )
+  end
+end
