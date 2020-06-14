153650fe6242c23a692e6c153fb0529f4aaae837
Artem Trepalin <atrepalin@simtechdev.com>
2020-06-14 15:47:12 +0300

    add_crypt_keeper_gem : add new migration for secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_093921) do
+ActiveRecord::Schema.define(version: 2020_06_14_093991) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,12 +78,13 @@ ActiveRecord::Schema.define(version: 2020_06_14_093921) do
     t.string "phone_number"
     t.date "birthday"
     t.string "address"
-    t.string "sickness"
+    t.text "sickness"
     t.integer "height"
     t.integer "weight"
     t.bigint "watcher_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["watcher_id"], name: "index_patients_on_watcher_id"
   end
 
